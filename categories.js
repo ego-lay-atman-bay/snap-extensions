b1a5d3f2f6fe369cb765a8896d375a852431a4c6
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2021-08-07 22:41:38 -0500

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,13 @@
+SnapExtensions.primitives.set(
+    'cat_del(name)',
+    function (name) {
+        world.children[0].deletePaletteCategory(name);
+    }
+);
+
+SnapExtensions.primitives.set(
+    'cat_add(name,r,g,b)',
+    function (name,r,g,b) {
+        world.children[0].addPaletteCategory(name, new Color(r,g,b));
+    }
+);
