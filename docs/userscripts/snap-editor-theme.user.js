065c85c413289dde29ea28a3f649acd48e28ffd9
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2022-09-02 20:32:27 -0600

    Add theme saving

--- docs/userscripts/snap-editor-theme.user.js
+++ docs/userscripts/snap-editor-theme.user.js
@@ -10,17 +10,34 @@
 // ==/UserScript==
 (function () {
     'use strict';
+    console.log('Injecting theme');
 
     MorphicPreferences.isLight = MorphicPreferences.isFlat
+
+    function update_settings() {
+        var settings = IDE_Morph.prototype.applySavedSettings.toString();
+        var settings_lines = settings.split('\n');
+        
+        var get_light_mode = "        theme = this.getSetting('theme');\n\n    if (theme === 'light') {\n        this.setLightDesign();\n    } else if (theme == 'dark') {\n        this.setDarkDesign();\n    } else {\n        if (design == 'flat') {\n            this.setLightDesign();\n        } else {\n            this.setDarkDesign();\n        }\n    }";
+
+        settings_lines.splice(settings_lines.indexOf(''), 0, get_light_mode);
+
+        const settings_func = new Function('IDE_Morph.prototype.applySavedSettings = ' + settings_lines.join('\n'));
+        settings_func();
+
+        IDE_Morph.prototype.applySavedSettings();
+    }
 
     IDE_Morph.prototype.darkDesign = function () {
         this.setDarkDesign();
         this.refreshIDE();
+        this.saveSetting('theme', 'dark');
     };
-
+    
     IDE_Morph.prototype.lightDesign = function () {
         this.setLightDesign();
         this.refreshIDE();
+        this.saveSetting('theme', 'light');
     };
 
     function replace_flat(text) {
@@ -36,17 +53,27 @@
         activate();
     }
 
+    function inject_design() {
+        var injection = "    if (MorphicPreferences.isLight) {\n        this.saveSetting('theme', 'light');\n    } else {\n        this.saveSetting('theme', 'dark');\n    }";
+
+        var flatdesign = IDE_Morph.prototype.flatDesign.toString().split('\n');
+        var defaultdesign = IDE_Morph.prototype.defaultDesign.toString().split('\n');
+
+        flatdesign.splice(flatdesign['length']-1, 0, injection);
+        defaultdesign.splice(defaultdesign['length']-1, 0, injection);
+
+        const injected_flat = new Function('IDE_Morph.prototype.flatDesign = ' + flatdesign.join('\n'));
+        const injected_default = new Function('IDE_Morph.prototype.defaultDesign = ' + defaultdesign.join('\n'));
+        
+        injected_flat();
+        injected_default();
+    }
+
+    inject_design();
+
     set_func('IDE_Morph.prototype.createLogo');
-    
-    // var logo = replace_flat(IDE_Morph.prototype.createLogo.toString());
-    // const setlogo = new Function('IDE_Morph.prototype.createLogo = ' + logo);
-    // setlogo();
 
     set_func('IDE_Morph.prototype.createControlBar');
-
-    // var controlbar = replace_flat(IDE_Morph.prototype.createControlBar.toString());
-    // const setcontrolbar = new Function('IDE_Morph.prototype.createControlBar = ' + controlbar);
-    // setcontrolbar();
 
     set_func('IDE_Morph.prototype.createCategories');
     set_func('IDE_Morph.prototype.createSpriteBar');
@@ -70,11 +97,14 @@
         MorphicPreferences.isFlat = false;
         IDE_Morph.prototype.scriptsPaneTexture = this.scriptsTexture();
     };
-    console.log('Injecting menu items');
+    
     var menu = IDE_Morph.prototype.settingsMenu.toString();
     var split = menu.split("\n    addPreference(\n        'Flat design',\n        () => {\n            if (MorphicPreferences.isFlat) {\n                return this.defaultDesign();\n            }\n            this.flatDesign();\n        },\n        MorphicPreferences.isFlat,\n        'uncheck for default\\nGUI design',\n        'check for alternative\\nGUI design',\n        false\n    );");
-    split.splice(1, 0, "    addPreference(\n        'Flat design',\n        () => {\n            if (MorphicPreferences.isFlat) {                return this.defaultDesign();\n            }\n            this.flatDesign();\n        },        MorphicPreferences.isFlat,\n        'uncheck for default\\nGUI design',        'check for alternative\\nGUI design',\n        false\n    );\n	addPreference(\n        'Dark design',\n        () => {\n            if (MorphicPreferences.isLight) {\n                return this.darkDesign();\n            }\n            this.lightDesign();\n        },\n        !MorphicPreferences.isLight,\n        'uncheck for default\\nGUI design',\n        'check for alternative\\nGUI design',\n        false\n    );");
+    split.splice(1, 0, "    addPreference(\n        'Flat design',\n        () => {\n            if (MorphicPreferences.isFlat) {                return this.defaultDesign();\n            }\n            this.flatDesign();\n        },        MorphicPreferences.isFlat,\n        'uncheck for default\\nGUI design',        'check for alternative\\nGUI design',\n        false\n    );\n	addPreference(\n        'Dark theme',\n        () => {\n            if (MorphicPreferences.isLight) {\n                return this.darkDesign();\n            }\n            this.lightDesign();\n        },\n        !MorphicPreferences.isLight,\n        'uncheck for light\\nGUI theme',\n        'check for dark\\nGUI theme',\n        false\n    );");
     var newFun = split.join('');
     const dark_mode = new Function('IDE_Morph.prototype.settingsMenu = ' + newFun);
     dark_mode();
+    
+    update_settings();
+    
 })();
