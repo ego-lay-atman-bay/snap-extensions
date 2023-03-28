3069ede8efaae7197ef57741363ab782de0d3b75
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2023-03-28 17:28:11 -0600

    Update select-categories to actually work.
    It didn't work when exporting variables were added.

--- docs/userscripts/select-categories.user.js
+++ docs/userscripts/select-categories.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Select Categories
 // @namespace    https://ego-lay-atman-bay.github.io/snap-extensions
-// @version      0.2
+// @version      0.3
 // @description  Adds the ability to select and deselect categories in the export and unused blocks dialogs.
 // @author       ego-lay-atman-bay
 // @match        https://snap.berkeley.edu/*/snap.html
@@ -13,33 +13,52 @@
 
     function checkVersion() {
         if (typeof SnapVersion !== 'undefined') {
-            let version = SnapVersion.split('.')
-            return [version[0], version[1]].join('.')
+            let version = SnapVersion.split('.');
+            return [version[0], version[1]].join('.');
         }
         else {
-            return false
-        }
-    }
+            return false;
+        };
+    };
 
     if (checkVersion() >= 7 || true) {
 
-        console.log('adding selecting categories')
+        function injectSelectCategories(func) {
+            let split = func.split('lastCat,');
+            split.splice(1, 0, 'lastCat, catCheckBox');
+            func = split.join('');
 
-        let func = BlockExportDialogMorph.prototype.buildContents.toString()
+            let start = func.indexOf('if (lastCat && (category !== lastCat))');
 
-        let split = func.split('lastCat,')
-        split.splice(1, 0, 'lastCat, catCheckBox')
-        func = split.join('')
+            if (start == -1) {
+                return false;
+            };
 
-        let start = func.indexOf('if (lastCat && (category !== lastCat))')
-        let end = func.indexOf('}', start)
+            let end = func.indexOf('}', start);
 
-        let injectStr = "if (category !== lastCat) {\n                    y += padding;\n                    catCheckBox = new ToggleMorph(\n                        'checkbox',\n                        this,\n                        () => {\n                            var blocks = [];\n                            if (contains(this.blocks.map(b => b.category), category)) {\n                                this.blocks.forEach(block => {\n                                    if (block.category != category) {\n                                        blocks.push(block)\n                                    };\n                                });\n                                this.blocks = blocks;\n                            }\n                            else {\n                                this.body.contents.children.forEach(block => {\n                                    if (block instanceof ToggleMorph) {\n                                        if (block.element instanceof CustomReporterBlockMorph || block.element instanceof CustomCommandBlockMorph) {\n                                            if (block.element.category == category) {\n                                                if (!contains(this.blocks, block.element.definition)) {\n                                                    block.trigger()\n                                                };\n                                            };\n                                        };\n                                    };\n                                });\n                            };\n                            try {\n                                this.collectDependencies(); // v8.0+\n                            }\n                            catch (err) { // v7\n                                this.body.contents.children.forEach(checkBox => {\n                                    if (checkBox instanceof ToggleMorph) {\n                                        checkBox.refresh();\n                                    }\n                                });\n                            }\n\n                        },\n                        // category,\n                        null,\n                        () => contains(this.blocks.map(b => b.category), category),\n                        null,\n                        null                        \n                    )\n                    // catCheckBox.label.color = new Color(255, 255, 255, 1)\n                    // catCheckBox.label.fontSize = 12\n                    // catCheckBox.label.setWidth()\n                    // catCheckBox.label.setTop()\n                    // catCheckBox.element.setTop(-(catCheckBox.fullBounds().height()/2))\n                    catCheckBox.setPosition(new Point(\n                        x,\n                        y + (catCheckBox.top())\n                    ));\n                    palette.addContents(catCheckBox);\n                    txt = SpriteMorph.prototype.categoryText(category);\n                    txt.setPosition(new Point(x + catCheckBox.fullBounds().width() + padding, y));\n                    txt.refresh = function() {};\n                    palette.addContents(txt);\n                    y += catCheckBox.fullBounds().height()\n                    y += padding\n                }"
+            let injectStr = "if (category !== lastCat) {\n                    y += padding;\n                    catCheckBox = new ToggleMorph(\n                        'checkbox',\n                        this,\n                        () => {\n                            var blocks = [];\n                            if (contains(this.blocks.map(b => b.category), category)) {\n                                this.blocks.forEach(block => {\n                                    if (block.category != category) {\n                                        blocks.push(block)\n                                    };\n                                });\n                                this.blocks = blocks;\n                            }\n                            else {\n                                this.body.contents.children.forEach(block => {\n                                    if (block instanceof ToggleMorph) {\n                                        if (block.element instanceof CustomReporterBlockMorph || block.element instanceof CustomCommandBlockMorph) {\n                                            if (block.element.category == category) {\n                                                if (!contains(this.blocks, block.element.definition)) {\n                                                    block.trigger()\n                                                };\n                                            };\n                                        };\n                                    };\n                                });\n                            };\n                            try {\n                                this.collectDependencies(); // v8.0+\n                            }\n                            catch (err) { // v7\n                                this.body.contents.children.forEach(checkBox => {\n                                    if (checkBox instanceof ToggleMorph) {\n                                        checkBox.refresh();\n                                    }\n                                });\n                            }\n\n                        },\n                        // category,\n                        null,\n                        () => contains(this.blocks.map(b => b.category), category),\n                        null,\n                        null                        \n                    )\n                    // catCheckBox.label.color = new Color(255, 255, 255, 1)\n                    // catCheckBox.label.fontSize = 12\n                    // catCheckBox.label.setWidth()\n                    // catCheckBox.label.setTop()\n                    // catCheckBox.element.setTop(-(catCheckBox.fullBounds().height()/2))\n                    catCheckBox.setPosition(new Point(\n                        x,\n                        y + (catCheckBox.top())\n                    ));\n                    palette.addContents(catCheckBox);\n                    txt = SpriteMorph.prototype.categoryText(category);\n                    txt.setPosition(new Point(x + catCheckBox.fullBounds().width() + padding, y));\n                    txt.refresh = function() {};\n                    palette.addContents(txt);\n                    y += catCheckBox.fullBounds().height()\n                    y += padding\n                }";
 
-        var newFunc = [func.substring(0, start), injectStr, func.substring(end + 1, func.length)].join('')
-        const injectFunc = new Function('BlockExportDialogMorph.prototype.buildContents = ' + newFunc)
-        injectFunc()
+            var newFunc = [func.substring(0, start), injectStr, func.substring(end + 1, func.length)].join('');
+            
+            return newFunc;
+        };
 
-        BlockRemovalDialogMorph.prototype.buildContents = BlockExportDialogMorph.prototype.buildContents;
-    }
+        console.log('adding selecting categories');
+
+        let exportDialog = BlockExportDialogMorph.prototype.buildContents.toString();
+        let unusedDialog = BlockRemovalDialogMorph.prototype.buildContents.toString();
+
+        let newExportDialog = injectSelectCategories(exportDialog);
+        let newUnusedDialog = injectSelectCategories(unusedDialog);
+
+        if (newExportDialog) {
+            const injectExport = new Function('BlockExportDialogMorph.prototype.buildContents = ' + newExportDialog);
+            injectExport();
+        };
+
+        if (newUnusedDialog) {
+            const injectUnused = new Function('BlockRemovalDialogMorph.prototype.buildContents = ' + newUnusedDialog);
+            injectUnused();
+        };
+    };
 })();
